ca36db079446b1d5070dc30fdc418d8132e83ef7
ec-han <onyxonxy@gmail.com>
2020-05-06 21:21:59 +0900

    add nodemailer

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const port = 5000
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const config = require('./config/key')
+const nodemailer = require('nodemailer')
 
-
-// application/x-www-form-urlencoded 이렇게 된 데이터를 분석해서 가져올 수 있게 해 줌
 app.use(bodyParser.urlencoded({ extended: true }))
-// application/json 타입으로 된 것을 분석해서 가져올 수 있게 해 줌
+
 app.use(bodyParser.json())
 
 mongoose.connect(config.mongoURI, {
@@ -20,4 +19,4 @@ mongoose.connect(config.mongoURI, {
 app.get('/', (req, res) => res.send('Hello World!'))
 
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Server is running on PORT: ${port}`))
